883dad1c047f792c5747984b0c7ba8374d5c6aec
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-13 13:29:43 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+void mensajeDeBienvenida(){
+	printf("Bienvenido a su barco!\n");
+}
+
+void imprimirPosicion(int x, int y){
+	printf("Hola! Tu barco esta en la posicion: %d, %d\n", x, y);
+}
+
+void navegarAlSur(){
+	printf("Navegando al SUR\n");
+	x++;
+	y+=10;
+}
+
+int main(){
+	int x = 0;
+	int y = 0;
+	
+	mensajeDeBienvenida();
+	imprimirPosicion();
+	
+	navegarAlSur();
+	imprimirPosicion();
+	
+	getch();
+	return 0;
+}
