2ba8ec41067bbcb2c7c09612361946ef581073a3
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-13 13:40:17 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void imprimirPosicion(int x, int y){
 	printf("Hola! Tu barco esta en la posicion: %d, %d\n", x, y);
 }
 
-void navegarAlSur(){
+void navegarAlSur(int x, int y){
 	printf("Navegando al SUR\n");
 	x++;
 	y+=10;
@@ -19,11 +19,15 @@ int main(){
 	int x = 0;
 	int y = 0;
 	
-	mensajeDeBienvenida();
-	imprimirPosicion();
+	printf("Direccion de x: %p \n", &x);
 	
-	navegarAlSur();
-	imprimirPosicion();
+	int *direccion_x = &x;
+	printf("Direccion de x: %p \n", direccion_x);
+	mensajeDeBienvenida();
+	imprimirPosicion(x, y);
+	
+	navegarAlSur(x, y);
+	imprimirPosicion(x, y);
 	
 	getch();
 	return 0;
