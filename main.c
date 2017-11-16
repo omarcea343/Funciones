fab580e5b4cb735c015cef0338709cb271225efe
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-16 12:01:18 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "funcionesBarco.h"
 
-void mensajeDeBienvenida(){
-	printf("Bienvenido a su barco!\n");
-}
-
-void imprimirPosicion(int x, int y){
-	printf("Hola! Tu barco esta en la posicion: %d, %d\n", x, y);
-}
-
-void navegarAlSur(int x, int y){
-	printf("Navegando al SUR\n");
-	x++;
-	y+=10;
-}
-
-int main(){
-	int x = 0;
-	int y = 0;
-	
-	printf("Direccion de x: %p \n", &x);
-	
-	int *direccion_x = &x;
-	printf("Direccion de x: %p \n", direccion_x);
-	mensajeDeBienvenida();
-	imprimirPosicion(x, y);
-	
-	navegarAlSur(x, y);
-	imprimirPosicion(x, y);
-	
-	getch();
-	return 0;
+int main() {
+    int x = 0;
+    int y = 0;
+    
+    bienvenida();
+    imprimirPosicion(x, y);
+    
+    navegarAlSur(&x, &y);
+    
+    imprimirPosicion(x, y);
+    
+    printf("El valor de X es: %d \n", x);
+    printf("La direccion de X es: %p \n", &x);
+    int *direccion_x = &x;
+    printf("La direccion de X guardada en otra variable es: %p \n", direccion_x);
+    
+    navegarAlSur(&x, &y);
+    imprimirPosicion(x, y);
+    
+    return EXIT_SUCCESS;
 }
