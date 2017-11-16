71db23c8f0c365605726aaa99125a1a47ff4146a
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-16 12:01:38 -0600

    Create funcionesBarco.h

---
+++ funcionesBarco.h
@@ -0,0 +1,12 @@
+#ifndef funcionesBarco_h
+#define funcionesBarco_h
+
+#include <stdio.h>
+
+void bienvenida();
+
+void navegarAlSur(int *x, int *y);
+
+void imprimirPosicion(int x, int y);
+
+#endif /* funcionesBarco_h */
